a00c0e4a806bcde0f2795215643ebac4385e6710
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2026-04-10 22:17:56 +0200

    initial commit

---
+++ src/main/java/cryptator/api/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * This file is part of cryptator-api, https://github.com/arnaud-m/cryptator-api
+ *
+ * Copyright (c) 2026-2026, Université Côte d'Azur. All rights reserved.
+ *
+ * Licensed under the BSD 3-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
+package cryptator.api;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Cryptator !");
+	}
+
+}
---
+++ src/test/java/cryptator/api/HelloTest.java
@@ -0,0 +1,22 @@
+/*
+ * This file is part of cryptator-api, https://github.com/arnaud-m/cryptator-api
+ *
+ * Copyright (c) 2026-2026, Université Côte d'Azur. All rights reserved.
+ *
+ * Licensed under the BSD 3-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
+package cryptator.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void test() {
+		assertEquals(4, 2 + 2);
+	}
+
+}
