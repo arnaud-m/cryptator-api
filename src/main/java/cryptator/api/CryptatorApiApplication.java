5226828a63cde07d1a913eb9394e26cbe577f804
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2026-04-11 18:19:51 +0200

    add dependencies and basic Spring Boot application with health endpoint

---
+++ src/main/java/cryptator/api/CryptatorApiApplication.java
@@ -0,0 +1,21 @@
+/*
+ * This file is part of cryptator-api, https://github.com/arnaud-m/cryptator-api
+ *
+ * Copyright (c) 2026-2026, Université Côte d'Azur. All rights reserved.
+ *
+ * Licensed under the BSD 3-clause license.
+ * See LICENSE file in the project root for full license information.
+ */
+package cryptator.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptatorApiApplication {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(CryptatorApiApplication.class, args);
+	}
+
+}
